27fa1c7ef6961f36b07989099f5736fe4d29a120
Cameron Nichol <99camnichol21@gmail.com>
2026-03-18 02:35:06 -0400

    Initial movie app

--- src/App.tsx
+++ src/App.tsx
@@ -3,47 +3,68 @@ import {useEffect,useState} from 'react';
 type MovieType = {
   Title: string;
   Year: string;
+  Poster: string;
+  imdbID: string;
 };
 
 function App() {
   const [movies, setMovies] = useState<MovieType[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
-  useEffect(() => {
-    async function fetchMovies() {
-      const res = await fetch(`https://www.omdbapi.com/?s=batman&apikey=${import.meta.env.VITE_API_KEY}`);
+  async function fetchMovies(query: string) {
+      const res = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=${import.meta.env.VITE_API_KEY}`);
       const data = await  res.json();
       
       if (data.Search) {
         setMovies(data.Search);
       } else {
-      console.log("No results:", data);
+        console.log("No results:", data);
       }
     }
 
-    fetchMovies();
-
-
+  useEffect(() => {
+    fetchMovies(searchTerm);
   }, []);
 
-  const titles = movies.map((movie) => 
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    fetchMovies(searchTerm);
+  }
+
+  
+
+  const films = movies.map((movie) => 
     (<Movie 
-      key={movie.Title}
+      key={movie.imdbID}
       title={movie.Title}
       year={Number(movie.Year)}
+      poster={movie.Poster}
     />));
+
   return (
     <div>
       <h1>My Movie App 🎬</h1>
-      {titles}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for a movie"
+        />
+        <button type="submit">Search</button>
+      </form>
+      {films}
     </div>
   );
 }
 
-function Movie(props: { title: string; year: number}) {
+function Movie(props: { title: string; year: number; poster: string}) {
   return (
     <div>
       <h2>{props.title}</h2>
       <p>{props.year}</p>
+      <img src={props.poster}></img>
     </div>
   );
 }
